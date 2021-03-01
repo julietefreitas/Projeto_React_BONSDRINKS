8967f8166757af6874e8e0bdfe3f8fc363aa764d
julietefreitas <juliete.gomes17@gmail.com>
2021-03-01 13:43:06 -0300

    + estilização formulario

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,11 @@
 import React, { Component } from "react";
-
+import styles from "./Footer.module.css";
 export default class Footer extends Component {
 	render() {
-		return <footer className="footer">Resilia 2020</footer>;
+		return (
+			<footer className={styles.footer}>
+				Resilia 2020 - Alguns direitos reservados
+			</footer>
+		);
 	}
 }
