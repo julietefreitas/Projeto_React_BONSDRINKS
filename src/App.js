57a29d1f48dacb89a4c40ccd7b476042682e282b
julietefreitas <juliete.gomes17@gmail.com>
2021-03-03 21:31:51 -0300

    + foto da aba de contatos

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import './App.css';
-import Footer from './components/Footer';
-import MenuHeader from './components/MenuHeader';
-import Formulario from './components/Formulario';
+import "./App.css";
+import Footer from "./components/Footer/Footer";
+import MenuHeader from "./components/Header/MenuHeader";
+import Formulario from "./components/Formulario/Formulario";
+import ImagemContato from "./components/ImagemContato/ImagemContato";
+import logoContato from "./imagens/contact-pic.jpg";
 
 function App() {
-  return (
-  
-    <div className="App">
-      <MenuHeader/>
-      <Formulario/>
-      <Footer/>
-    {/*   <MaisJSX/> */}
-    </div>
-  );
+	return (
+		<div className="App">
+			<MenuHeader />
+			<Formulario />
+			<Footer />
+			<ImagemContato foto={logoContato} />
+		</div>
+	);
 }
 
 export default App;
