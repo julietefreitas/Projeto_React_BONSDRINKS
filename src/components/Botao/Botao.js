f1259989729512486b161423e711ec53921f0276
julietefreitas <juliete.gomes17@gmail.com>
2021-03-06 20:28:02 -0300

    + this state disabled e troca texto ao enviar

--- src/components/Botao/Botao.js
+++ src/components/Botao/Botao.js
@@ -2,10 +2,38 @@ import React, { Component } from "react";
 import styles from "./Botao.module.css";
 
 export default class Botao extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			disabled: false,
+			nome: "Enviar",
+		};
+		this.handleClick = this.handleClick.bind(this);
+	}
+	handleClick(e) {
+		e.preventDefault();
+		console.log("oi");
+		this.setState(() => ({
+			disabled: true,
+			nome: "Enviando...",
+		}));
+		setTimeout(() => {
+			this.setState(() => ({
+				disabled: false,
+				nome: "Enviar",
+			}));
+		}, 3000);
+	}
+
 	render() {
 		return (
-			<button type="submit" className={styles.input4}>
-				Enviar
+			<button
+				type="submit"
+				disabled={this.state.disabled}
+				className={styles.input4}
+				onClick={this.handleClick}
+			>
+				{this.state.nome}
 			</button>
 		);
 	}
