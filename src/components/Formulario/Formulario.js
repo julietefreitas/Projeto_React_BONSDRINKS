605c36664aecbd6b98370aff80ed6127e401247a
julietefreitas <juliete.gomes17@gmail.com>
2021-03-06 20:26:52 -0300

    + classes duplas nas tags

--- src/components/Formulario/Formulario.js
+++ src/components/Formulario/Formulario.js
@@ -1,22 +1,29 @@
 import React, { Component } from "react";
 import Botao from "../Botao/Botao";
 import styles from "./Formulario.module.css";
+import ImagemContato from ".././ImagemContato/ImagemContato";
+import logoContato from "../../imagens/contact-pic.jpg";
 
 export default class Formulario extends Component {
 	render() {
 		return (
-			<div className={styles.forms}>
+			<div className={styles.main}>
 				<h1 className={styles.titulo}>Contato</h1>
+				<ImagemContato foto={logoContato} />
 				<form className={styles.AppForm}>
-					<label className={(styles.LabelForm, styles.label1)}>Nome</label>
+					<label className={styles.LabelForm + " " + styles.label1}>Nome</label>
 					<br></br>
-					<input className={(styles.InputForm, styles.input1)} />
+					<input className={styles.InputForm + " " + styles.input1} />
 					<br></br>
-					<label className={(styles.LabelForm, styles.label2)}>Email</label>
+					<label className={styles.LabelForm + " " + styles.label2}>
+						Email
+					</label>
 					<br></br>
-					<input className={(styles.InputForm, styles.input2)} />
+					<input className={styles.InputForm + " " + styles.input2} />
 					<br></br>
-					<label className={(styles.LabelForm, styles.label3)}>Mensagem</label>
+					<label className={styles.LabelForm + " " + styles.label3}>
+						Mensagem
+					</label>
 					<br></br>
 					<input className={styles.input3}></input>
 					<Botao />
