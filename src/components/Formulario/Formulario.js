c92a80daf11bce557741a04604b83926989921f2
julietefreitas <juliete.gomes17@gmail.com>
2021-03-10 12:25:44 -0300

    + Input e label como componentes de input

--- src/components/Formulario/Formulario.js
+++ src/components/Formulario/Formulario.js
@@ -1,34 +1,22 @@
 import React, { Component } from "react";
 import Botao from "../Botao/Botao";
 import styles from "./Formulario.module.css";
-import ImagemContato from ".././ImagemContato/ImagemContato";
+import Imagem from "../Imagem/Imagem";
 import logoContato from "../../imagens/contact-pic.jpg";
+import Input from "../../Input/Input";
 
-export default class Formulario extends Component {
-	render() {
-		return (
-			<div className={styles.main}>
-				<h1 className={styles.titulo}>Contato</h1>
-				<ImagemContato foto={logoContato} />
-				<form className={styles.AppForm}>
-					<label className={styles.LabelForm + " " + styles.label1}>Nome</label>
-					<br></br>
-					<input className={styles.InputForm + " " + styles.input1} />
-					<br></br>
-					<label className={styles.LabelForm + " " + styles.label2}>
-						Email
-					</label>
-					<br></br>
-					<input className={styles.InputForm + " " + styles.input2} />
-					<br></br>
-					<label className={styles.LabelForm + " " + styles.label3}>
-						Mensagem
-					</label>
-					<br></br>
-					<input className={styles.input3}></input>
-					<Botao />
-				</form>
-			</div>
-		);
-	}
+function Formulario() {
+	return (
+		<div className={styles.main}>
+			<h1 className={styles.titulo}>Contato</h1>
+			<Imagem foto={logoContato} />
+			<form className={styles.AppForm}>
+				<Input infoLabel="label1" input="input1" children="Nome" />
+				<Input infoLabel="label2" input="input2" children="Email" />
+				<Input infoLabel="label3" input="input3" children="Mensagem" />
+				<Botao />
+			</form>
+		</div>
+	);
 }
+export default Formulario;
