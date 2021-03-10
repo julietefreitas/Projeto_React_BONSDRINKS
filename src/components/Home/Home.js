9779c542eddf18facc26a35abb62233e19b9fe89
julietefreitas <juliete.gomes17@gmail.com>
2021-03-10 12:29:53 -0300

    + adicao da componente imagem

---
+++ src/components/Home/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Imagem from "../Imagem/Imagem";
+
+function Home() {
+	return (
+		<>
+			<Imagem
+				foto="https://www.familypower-blog.de/wp-content/uploads/bb-plugin/cache/kaizen-nguy-n-jcLcWL8D7AQ-unsplash-landscape.jpg"
+				classe="imagemHome"
+			/>
+		</>
+	);
+}
+
+export default Home;
