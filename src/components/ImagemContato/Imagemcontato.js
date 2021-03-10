6bc0c0e41b9c4f444bfd75b93dbae6a0a2c71deb
julietefreitas <juliete.gomes17@gmail.com>
2021-03-10 12:24:39 -0300

    + componente Imagem correto

--- src/components/ImagemContato/Imagemcontato.js
+++
@@ -1,8 +0,0 @@
-import React, { Component } from "react";
-import styles from "./ImagemContato.module.css";
-
-export default class Imagemcontato extends Component {
-	render() {
-		return <img className={styles.imgContato} src={this.props.foto} />;
-	}
-}
