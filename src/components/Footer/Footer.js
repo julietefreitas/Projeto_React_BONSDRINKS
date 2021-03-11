c16943665ab015dd9264328f6f917328b6f94ffb
julietefreitas <juliete.gomes17@gmail.com>
2021-03-11 10:15:40 -0300

    + mudanca p componente funcional

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,12 @@
-import React, { Component } from "react";
+import React from "react";
 import styles from "./Footer.module.css";
-export default class Footer extends Component {
-	render() {
-		return (
-			<footer className={styles.footer}>
-				<p>Resilia 2020 - Alguns direitos reservados</p>
-			</footer>
-		);
-	}
-}
+
+const Footer = () => {
+	return (
+		<footer className={styles.footer}>
+			<p>Resilia 2020 - Alguns direitos reservados</p>
+		</footer>
+	);
+};
+
+export default Footer;
