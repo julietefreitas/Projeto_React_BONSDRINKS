b12118814a16260c7c73bd5e686197b7530efba1
julietefreitas <juliete.gomes17@gmail.com>
2021-03-11 10:18:14 -0300

    + componente funcional

--- src/components/Formulario/Formulario.js
+++ src/components/Formulario/Formulario.js
@@ -3,20 +3,44 @@ import Botao from "../Botao/Botao";
 import styles from "./Formulario.module.css";
 import Imagem from "../Imagem/Imagem";
 import logoContato from "../../imagens/contact-pic.jpg";
-import Input from "../../Input/Input";
+import Input from "../Input/Input";
 
-function Formulario() {
+const Formulario = () => {
 	return (
 		<div className={styles.main}>
 			<h1 className={styles.titulo}>Contato</h1>
 			<Imagem foto={logoContato} />
 			<form className={styles.AppForm}>
-				<Input infoLabel="label1" input="input1" children="Nome" />
-				<Input infoLabel="label2" input="input2" children="Email" />
-				<Input infoLabel="label3" input="input3" children="Mensagem" />
+				<Input
+					htmlFor="fnome"
+					id="fname"
+					name="fname"
+					type="text"
+					label="label1"
+					input="input1"
+					children="Nome"
+				/>
+				<Input
+					htmlFor="femail"
+					id="femail"
+					name="femail"
+					type="text"
+					label="label2"
+					input="input2"
+					children="Email"
+				/>
+				<Input
+					htmlFor="fmsg"
+					id="fmsg"
+					name="fmsg"
+					type="text"
+					label="label3"
+					input="input3"
+					children="Mensagem"
+				/>
 				<Botao />
 			</form>
 		</div>
 	);
-}
+};
 export default Formulario;
