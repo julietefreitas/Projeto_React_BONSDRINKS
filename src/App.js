2ce05e12b603798ba9355569b3b9a4625c087c09
julietefreitas <juliete.gomes17@gmail.com>
2021-03-13 17:25:36 -0300

    + react router dom, links com a navbar

--- src/App.js
+++ src/App.js
@@ -2,18 +2,37 @@ import "./App.css";
 import Footer from "./components/Footer/Footer";
 import MenuHeader from "./components/Header/MenuHeader";
 import Formulario from "./components/Formulario/Formulario";
-import ImagemContato from "./components/ImagemContato/ImagemContato";
-import logoContato from "./imagens/contact-pic.jpg";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import SobreNos from "./components/SobreNos/SobreNos";
+import Home from "./components/Home/Home";
+import Drinks from "./components/Drinks/Drinks";
 
-function App() {
+const App = () => {
 	return (
 		<div className="App">
-			<MenuHeader />
-			<Formulario />
-			<Footer />
-			<ImagemContato foto={logoContato} />
+			<Router>
+				<MenuHeader />
+				<Switch>
+					<Route exact path="/">
+						<Home />
+					</Route>
+					<Route exact path="/contato">
+						<Formulario />
+					</Route>
+					<Route exact path="/sobre">
+						<SobreNos />
+					</Route>
+					<Route exact path="/nossoTime">
+						<SobreNos />
+					</Route>
+					<Route exact path="/drinks">
+						<Drinks />
+					</Route>
+				</Switch>
+				<Footer />
+			</Router>
 		</div>
 	);
-}
+};
 
 export default App;
