7cd57d53466c028b337cb247da0d557bea6fb1f9
julietefreitas <juliete.gomes17@gmail.com>
2021-03-28 22:32:19 -0300

    + rotas de pop e buscas

--- src/components/Drinks/Drinks.js
+++ src/components/Drinks/Drinks.js
@@ -1,8 +1,13 @@
 import React, { useState, useEffect } from "react";
+import { Link, Route , Switch} from 'react-router-dom';
+import Card from "../Card/Card";
+import Botao from "../Botao/Botao";
+import DrinksPopulares from  "../DrinksPopulares/DrinksPopulares"
+import styles from "./Drinks.module.css"
 
 function Drinks() {
 	const [results, setResults] = useState([]);
-
+/* 
 	useEffect(() => {
 		fetch("https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail")
 			.then((response) => {
@@ -13,26 +18,45 @@ function Drinks() {
 				setResults(dados);
 			});
 	}, []);
+	useEffect(() => {}); */
 
 	return (
-		<div>
-			{results.map((drink) => {
-				return (
-					<div>
-						<img
-							style={{
-								width: "100px",
-								height: "100px",
-								borderRadius: "4px",
-							}}
-							src={drink.strDrinkThumb}
-						></img>
-						<p>{drink.strDrink}</p>
-					</div>
-				);
-			})}
+		<div className = {styles.container_drinks}>,
+		<h1 className = {'titulo' + " " + styles.titulo}>Drinks</h1>
+		<nav className= {'navBar' + " " + styles.navBar}>
+        <ul className={'btnUl' + " "+ styles.btnUl}>
+          <li className={'btn' + " "+ styles.btn}>
+            <Link to="/drinks/pop">
+            Drinks Populares
+            </Link>
+          </li>
+          <li className={'btn1' + " "+ styles.btn1}>
+            <Link to="/drinks/buscar">
+              Buscar Drinks
+            </Link>
+          </li>
+        </ul>
+      </nav>
+			<Switch>
+          <Route path="/drinks/pop" element={<DrinksPopulares />}>
+					</Route>
+          <Route path="/drinks/buscar" />
+        </Switch>
 		</div>
+		
 	);
 }
 
 export default Drinks;
+
+	/* style={{
+				display: "flex",
+				width: "75%",
+				flexWrap: "wrap",
+				justifyContent: "space-between",
+			}}
+		>
+			{results.map((drink) => {
+				return <Card drink={drink} />;
+			})}
+			<Botao /> */
